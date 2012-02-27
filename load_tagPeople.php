5593e6289702148f36d961c6139595df11e9973f
koma5 <marco@5th.ch>
2012-02-27 17:13:37 +0100

    got regex for hashtags working

--- load_tagPeople.php
+++ load_tagPeople.php
@@ -15,7 +15,7 @@ echo "<h1>tagPeople</h1>";
 
 include('lib/TwitterSearch.php');
 
-$search = new TwitterSearch('#ff');
+$search = new TwitterSearch('#tagPeople');
 $search->user_agent = '@tagPeople';
 
 $results = $search->results();
@@ -26,11 +26,14 @@ foreach($results as $i)
 	//echo $i->from_user . ": " . $i->text;
 
 	preg_match_all('/@([a-z0-9A-Z_]{1,15})/', $i->text, $taggedOnes);
-	preg_match_all('/#([a-z0-9A-Z_]{1,15})/', $i->text, $tags);
+	preg_match_all('/#([a-z0-9A-Z_]+)/', $i->text, $tags);
 	//var_dump($taggedOnes);
 
+	//$taggedOnes['0'] for the whole matched pattern / $taggedOnes['0'] for the matched pattern inside ()
 	echo $i->from_user. " tagged " . join(', ', $taggedOnes[0]) . " with " . join(', ', $tags[0]);
 
 	echo "<br />";
 }
+
+
 ?>
