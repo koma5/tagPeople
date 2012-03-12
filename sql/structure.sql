2bef0a7ed51cdcce1b68f99ee11dfc85bf384611
koma5 <marco@5th.ch>
2012-03-12 16:29:32 +0100

    simple db structure without tweet

--- sql/structure.sql
+++ sql/structure.sql
@@ -22,21 +22,31 @@ CREATE TABLE users
   INDEX (`id`)
 ) ENGINE=INNODB;
 
-CREATE TABLE tweets
-(
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `text` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX (`id`)
-) ENGINE=INNODB;
+#CREATE TABLE tweets
+#(
+#  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#  `text` INT UNSIGNED NOT NULL,
+#  PRIMARY KEY (`id`),
+#  INDEX (`id`)
+#) ENGINE=INNODB;
 
 CREATE TABLE user_tag
 (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `id_user` INT UNSIGNED NOT NULL,
   `id_tag` INT UNSIGNED NOT NULL,
-  `id_user_tagger` INT UNSIGNED NOT NULL,
-  `id_tweet` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   INDEX (`id`)
 ) ENGINE=INNODB;
+
+
+DROP VIEW IF EXISTS vTags;
+CREATE VIEW vTags
+AS
+  SELECT t.tag, u.username
+  FROM user_tag AS ut
+    INNER JOIN tags AS t
+    ON ut.id_tag = t.id
+    INNER JOIN users AS u
+    ON ut.id_user = u.id
+  ORDER BY t.tag;
